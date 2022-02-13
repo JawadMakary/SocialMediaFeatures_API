c9c615c6787db059b16702957748f4b9e3d9c69d
jawad halby makary <jawadhalbymakary@gmail.com>
2022-02-13 19:41:40 +0200

    added comment model

---
+++ models/commentModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const commentSchema = new Schema({
+    commentOwner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+       
+    },
+  parentPost: {
+    type: Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  commentContent: {
+    type: String,
+    default: "",
+    maxlength: [255, "content is too long"],
+    },
+
+
+
+
+},
+{ timestamps: true }
+);
+module.exports = mongoose.model("Comment", commentSchema);
